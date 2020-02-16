ea3d8ebde173054744477fcad297e90a51bf3a77
zmsl6089 <zmsl6089>
2020-02-16 20:29:25 +0200

    fetch cashcog data and save in in mysql

--- src/stream/cashcog.event.stream.api.js
+++ src/stream/cashcog.event.stream.api.js
@@ -1,17 +1,57 @@
 var https = require('https');
 var apiStream = require('api-stream');
-var apiURL = 'https://cashcog.xcnt.io/single';
+var mysql = require('mysql');
+var apiURL = 'https://cashcog.xcnt.io/stream';
+
+var dbConnection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    database: "xcnt"
+});
+
+dbConnection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 
 module.exports = apiStream.createApi(constructorOptions => (query, done) => {
     https.get(apiURL, res => {
-        var response = '';
 
         if (res.statusCode !== 200) {
             done({ status: res.statusCode }, null);
             return;
         }
 
-        res.on('data', d => response += d);
-        res.on('end', () => done(null, JSON.parse(response)));
+        res.on('data', data => {
+            try {
+                let event = JSON.parse(data.toString())
+
+                let employeeSql = `INSERT IGNORE INTO employees 
+                (uuid, first_name, last_name) 
+                VALUES ('${event.employee.uuid}', '${event.employee.first_name}', '${event.employee.last_name}')`;
+
+                dbConnection.query(employeeSql, function (err, result) {
+                    if (err) throw err;
+                    console.log("1 employee record inserted");
+                });
+
+                let eventSql = `INSERT INTO events 
+                (uuid, description, created_at, amount, currency, employee, status) 
+                VALUES ('${event.uuid}', '${event.description}', '${event.created_at}', ${event.amount}, '${event.currency}', '${event.employee.uuid}', 'PENDING')`;
+
+                dbConnection.query(eventSql, function (err, result) {
+                    if (err) throw err;
+                    console.log("1 event record inserted");
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        });
+
+        res.on('end', () => {
+            done(null, "DONE");
+            dbConnection.end();
+        });
     });
 });
--- src/stream/cashcog.event.stream.js
+++ src/stream/cashcog.event.stream.js
@@ -1,15 +1,11 @@
 var CashcogEventStreamApi = require('./cashcog.event.stream.api.js');
 
-// create an API stream which runs with a maximum of
-// 10 queries per second, and caches results in wiki.db
 var cashcogEventStream = new CashcogEventStreamApi({
-    queriesPerSecond: 10,
-    cacheFile: 'cashcog.db'
+    queriesPerSecond: 10
 });
 
-cashcogEventStream.on('data', d => console.log(d));
+cashcogEventStream.on('data', data => console.log(data));
 cashcogEventStream.on('end', () => console.log('Done.'));
 
 cashcogEventStream.write();
-
 cashcogEventStream.end();
